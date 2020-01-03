40e1864818d0d93c234dd2da575d57385af5baaf
Daisuke Saito <dsaito85a@gmail.com>
2020-01-03 11:32:48 +0900

    feat: 画像のアップロード機能の追加

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        \Log::info("post image");
+
+        if ($request->file('image') != null) {
+            // TODO: 本当に画像ファイルかどうか、のバリデーションを追加したい
+            if ($request->file('image')->isValid()) {
+                \App\Models\Image::create(
+                    ['name' => basename($request->image->store('public/image'))]
+                );
+                \Log::debug('アップロードした画像を保存しました');
+            } else {
+                \Log::debug('画像の保存に失敗しました');
+            }
+        } else {
+            \Log::debug('画像が指定されていません');
+        }
+
+        return view('welcome');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
