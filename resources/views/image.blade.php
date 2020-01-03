a0fce5d62b6417dddad8c15d27ddf3293a78b594
Daisuke Saito <dsaito85a@gmail.com>
2020-01-03 15:56:54 +0900

    feat: OGPを追加

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@daisuke7924" />
+    <meta name="twitter:creator" content="@daisuke7924" />
+    <meta property="og:url" content="{{url()->current()}}" />
+    <meta property="og:title" content="クソコラメーカー" />
+    <meta property="og:description" content="さぁみんなで、ごきげんになろう！" />
+    <meta property="og:image" content="{{url("/")}}/storage/image/{{$image->getFolder()}}/add.{{$image->getExtension()}}" />
+</head>
+<body>
 <a href="{{ url('/') }}">戻る</a>
 @if ($image != null)
 <p>合成した画像</p>
@@ -11,3 +26,5 @@
 <p>アップロードした画像</p>
 <img src="/storage/image/{{$image->name}}" alt="image" style="max-width:100%">
 @endif
+</body>
+</html>
