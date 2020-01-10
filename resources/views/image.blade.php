5c477a6237018dba579d0ac3e615547965d3841a
yamada <eri.genkidama@gmail.com>
2020-01-11 01:47:24 +0900

    クソコラを表示するエリアを作成

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -11,11 +11,29 @@
     <meta property="og:title" content="クソコラメーカー" />
     <meta property="og:description" content="さぁみんなで、ごきげんになろう！" />
     <meta property="og:image" content="{{url("/")}}/storage/image/{{$image->getFolder()}}/add.{{$image->getExtension()}}" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <a href="{{ url('/') }}">戻る</a>
 @if ($image != null)
 <p>合成した画像</p>
+<div class="kusocolla">
+  <div class="kusocolla__face face-01">
+    @for($i =0; $i<$image->face_num; $i++)
+        <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
+    @endfor
+  </div>
+  <div class="kusocolla__face face-02">
+    @for($i =0; $i<$image->face_num; $i++)
+        <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
+    @endfor
+  </div>
+  <div class="kusocolla__face face-03">
+    @for($i =0; $i<$image->face_num; $i++)
+        <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
+    @endfor
+  </div>
+</div>
 <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/add.{{$image->getExtension()}}" alt="image" style="max-width:100%">
 <p>顔だけ切り出した画像</p>
 @for($i =0; $i<$image->face_num; $i++)
